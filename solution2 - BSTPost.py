743caad39e0f55bb9f9177501a6a9b418b5f958e
Fin McCallum <finlaymac@outlook.com>
2020-06-02 19:29:12 +0100

    finished level 2

--- solution2 - BSTPost.py
+++ solution2 - BSTPost.py
@@ -1,22 +1,37 @@
-def solution(params):
-    treeSize = 2**params[0] - 1
-    values = params[1]
+def solution(height, values):
+    
+    #  find number of nodes in tree
+    treeSize = 2**height - 1
     parents = []
-    for val in values:
-        parents.append(findParent(val, treeSize, 1))
+    
+    #Recursive function to find children of nodes
+    def findChilds(val, root, prev, level):
+        #find the math remainder of the tree depending on the level currently on
+        remainder = 2**(height-level)-1
 
-    def findParent(val, root, level):
-        # if value is root of tree
-        if val == root:
-            return -1
-        prev = 0
+        #find left right and root of that level
+        left = remainder + prev
+        right = left + remainder
+        root = right + 1
         
-        while True:
-
-            # left of tree`
-            if val < root+1//2:
-                None
-            # right of tree
-            else:
-                None
-            
+        #check if direct children match value
+        if left == val or right == val:
+            return root
+        
+        #if the value is on the right side of the root node change the prev value
+        if val > left:
+            prev = left
+        
+        # recursively call the function
+        return findChilds(val, root, prev, level+1)
+    
+    # iterate through all the values and call the function
+    for val in values:
+        if val == treeSize:
+            parents.append(-1)
+        elif val > treeSize:
+            parents.append(-1)
+        else:
+            parents.append(findChilds(val, treeSize, 0, 1))
+    
+    return parents
